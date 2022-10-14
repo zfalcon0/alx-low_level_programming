44ac9426416400be3160b58747cc039976af1538
zfalcon0 <biruktelavive@gmaiel.com>
2022-10-14 11:43:51 -0700

    print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Prints the alphabetic
+*
+* Return: Always (Success)
+*/
+
+int main(void)
+{
+char a;
+for (a = a; a <= z; a++)
+putchar(a);
+putchar(n);
+return (0);
+}
