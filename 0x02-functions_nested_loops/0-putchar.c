5e356b69581d93ed00a91b237bd4427cae6df42c
zfalcon0 <biruktelavive@gmaiel.com>
2022-10-18 05:14:03 -0700

    m

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* main - entry point
+*
+* Description: Prints Holberton with _putchar
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+int i = 0;
+char c;
+char s[] = "_putchar\n";
+while (i <= 9)
+{
+c = s[i];
+_putchar(c);
+i++;
+}
+return (0);
+}
