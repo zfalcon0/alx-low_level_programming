e53c1b2e431befc74852f26c6c808f1a3e5cbe0e
zfalcon0 <biruktelavive@gmaiel.com>
2022-10-20 13:11:35 -0700

    task

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,12 @@
 
 void print_most_numbers(void)
 {
-int i = 0;
+int i = '0';
 
-for (; i <= 9; i++)
+for (; i <= '9'; i++)
 {
-if (i != 2 && i != 4)
+if (i != '2' && i != '4')
 _putchar(i);
 }
-_putchar(n);
+_putchar('\n');
 }
